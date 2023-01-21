596db893d40723cda4385c56536b89b1cf1090aa
SantAnna <brendathena@gmail.com>
2023-01-21 06:08:50 +0000

    Aula 4 - Final

--- NLW_Habits/script.js
+++ NLW_Habits/script.js
@@ -1,15 +1,28 @@
-const form = document.querySelector('form')
+const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
-button.addEventListener('click', add)
+// -- registro em memória --
+button.addEventListener('click', add) // -- registro em memória com evento de click --
+form.addEventListener('change', saved) // -- registro em memória com uma alterção change --
 
 function add() {
-    const today = "01/01"
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if(dayExists) {
+    alert("Dia já incluso")
+    return
+  }
 
-    const dayExists = nlwSetup.dayExists(today)
-
-    alert(dayExists)
-
-    nlwSetup.addDay("01/01")
+  alert('Adicionado com sucesso')
+  nlwSetup.addDay(today)
 }
+
+function saved() {
+  localStorage.setItem('NLWSetup@habtis', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habtis')) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
